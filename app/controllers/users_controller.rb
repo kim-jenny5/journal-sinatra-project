184e745649463cad5e6b05def12bf19beea37a19
Jenny Kim <kimj1905@gmail.com>
2021-06-07 14:17:19 -0400

    Added validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
     get '/signup' do
         erb :'/user/signup'
     end
+
+    post '/signup' do
+        # binding.pry
+        @user = User.create(params)
+
+        if @user.save
+            
+        else
+            redirect to "/signup"
+        end
+
+        # if @user.name.blank? || @user.username.blank? || @user.password.blank?
+        #     redirect to "/signup"
+        # else
+        #     redirect to "/users/index"
+        # end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
    has_many :entries
-
+   validates :name, :username, :password, presence: true
    has_secure_password
 end
