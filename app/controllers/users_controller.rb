14f091d1195ae4376660bb86008003f6b4aadf6d
Jenny Kim <kimj1905@gmail.com>
2021-06-07 13:45:34 -0400

    Set up user signup and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,6 @@
 class UsersController < ApplicationController
     
+    get '/signup' do
+        erb :'/user/signup'
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-   has_many :entries 
+   has_many :entries
+
+   has_secure_password
 end
