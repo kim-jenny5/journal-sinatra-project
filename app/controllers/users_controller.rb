13fb950b188eb130db0888350778b6b75bff2cab
Jenny Kim <kimj1905@gmail.com>
2021-06-08 19:41:54 -0400

    Fixing small issues with editing profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,13 @@ class UsersController < ApplicationController
     end
 
     post '/signup' do
-        @user = User.create(name: params[:name], username: params[:username], password: params[:username])
-
+        @user = User.create(name: params[:name], username: params[:username], password: params[:password])
+        current_user = @user
         if @user.save
             session[:user_id] = @user.id
             # This should redirect to user's home/'index' page with URL as user's username
             # redirect to "/#{@user.username}"
-            redirect to "/#{current_user.username}/home"
+            redirect to "/#{current_user.username}"
         else
             # flash message here that the username is taken
             redirect to "/signup"
@@ -28,9 +28,10 @@ class UsersController < ApplicationController
 
     post '/login' do
         @user = User.find_by_username(params[:username])
+        # binding.pry
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
-            redirect to "/#{current_user.username}/home"
+            redirect to "/#{current_user.username}"
         else
             # flash[:error] = "The email and/or password you entered is incorrect. Please enter the correct information."
             redirect to "/login"
@@ -62,9 +63,10 @@ class UsersController < ApplicationController
     patch '/:username/edit' do 
         if logged_in?
             @user = User.find_by_username(params[:username])
-            @user.update(name: params[:name], name: params[:username])
-            redirect to "/#{current_user.username}/edit"
-            # binding.pry
+            @user.name = params[:name]
+            @user.username = params[:username]
+            @user.save
+            redirect to "/#{current_user.username}"
         else
             redirect to "/login"
         end 
